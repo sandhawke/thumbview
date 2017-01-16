8cc1965551f1da040fe69300345092c47e6359ff
Sandro Hawke <sandro@hawke.org>
2017-01-16 18:47:40 -0500

    number keys = number of rows, working well

--- index.js
+++ index.js
@@ -36,8 +36,6 @@ function create () {
     thumbsize: 100,
     rows: 5,
     cols: 5,
-    // offset: 0
-    pageOffset: 0,
     row: 0,
     col: 0
     
@@ -58,20 +56,25 @@ function create () {
     const el = document.getElementById('thumbs')  //  DOMDRIVER USAGE ERROR
     if (el) {
       const w = el.clientWidth
-      const cols = Math.floor(w/(vs.thumbsize + 4))
+      const cols = Math.floor((w-6)/(vs.thumbsize + 4))
       if (cols != vs.cols) {
         vs.cols = cols
         console.log('resize', w, cols)
       }
     }
 
+    /*
     // const h = document.getElementById('thumbs').clientHeight
-    const h = 300
+    const h = 280
     const rows = Math.floor(h/(vs.thumbsize + 4))
     if (rows != vs.rows) {
       vs.rows = rows
       console.log('resize', h, rows)
     }
+    */
+    const h = 280
+    vs.thumbsize = Math.floor((h-4)/vs.rows)-4
+    console.log('thumbsize = ', vs.thumbsize)
 
     
   }
@@ -92,48 +95,44 @@ function create () {
   }
   setSize()
 
-  function thumbview (slides, config) {
+  let slides
+  let config
+  
+  function thumbview (slidesArg, configArg) {
+    // make these available to semi-global event handlers
+    // (maybe set that up when we're first called, instead)
+    slides = slidesArg
+    config = configArg
     // include 'vs' in among the objects domdriver will be watching
-    return [thumbview2, vs, slides, config]
+    return [thumbview2, vs]
   }
 
-  let updateSelection = () => {}
+  //  let updateSelection = () => {}
 
-  function thumbview2 (vs, slides, config) {
-    console.log('incoming offset', vs.pageOffset)
+  function thumbview2 () {
+    //console.log('incoming offset', vs.pageOffset)
 
     setSize()
-    
+
+    /*
     updateSelection = () => {
       let offset = vs.pageOffset
       if (offset < 0) offset += slides.length
       const item = slides[ offset + vs.row * vs.cols + vs.col ]
       config.selected = item
     }
+    */
     
     const out = ['div', { id:'x1', width: '100%' }]
     const pix = ['p', {}]
-    // const limit = vs.rows * vs.cols
-    let offset = vs.pageOffset
-    // negative offset, counting from the end, is good if slides are
-    // being inserted while we're watching, and we want to stay at the
-    // end, instead of staying at a fixed offset from the begining.
-    if (offset < 0) offset += slides.length
-    if (offset < 0) {
-      console.log('TOO FAR BACK')
-      vs.pageOffset = 1 - slides.length    
-    }
-    if (offset >= slides.length) {
-      console.log('TOO FAR FORWARD')
-      vs.pageOffset = slides.length - 1
-    }
-    offset = vs.pageOffset
-    if (offset < 0) offset += slides.length
+
+    let offset = pageOffset()
 
     for (let row = 0; row < vs.rows; row++) {
       for (let col = 0; col < vs.cols; col++) {
         const item = slides[ offset + row * vs.cols + col ]
-        const me = (row === vs.row && col === vs.col)
+        // const me = (row === vs.row && col === vs.col)
+        const me = (config.selected === item)
 
         // Call config.onclick(item) if item is clicked on.  So we need
         // a different onclick function for each item, bound to that
@@ -149,6 +148,7 @@ function create () {
         
         if (!item) {
           // console.log('no slide at', offset, row, col)
+          /*
           pix.push(['img',
                     {
                       height: vs.thumbsize,
@@ -156,10 +156,11 @@ function create () {
                       $border: me ? '2px solid blue' : '2px solid gray',
                       src: 'http://www.codeodor.com/images/Empty_set.png'
                     }])
+          */
         } else {
           if (item.photoKey) {
             pix.push(['img', {
-              id: 'img_' + item.photoKey,
+              //id: 'img_' + item.photoKey,
               height: vs.thumbsize,
               width: vs.thumbsize,
               onclick: onclick,
@@ -175,23 +176,17 @@ function create () {
       pix.push(['br'])
     }
     out.push(pix)
-    out.push(vs.pageOffset)
+    {
+      const i = indexOfSelected()
+      const count = slides.length
+      const pageSize = vs.rows * vs.cols
+      const pageNum = Math.floor(indexOfSelected() / pageSize)
+      const pages = Math.floor(count / pageSize) + 1
+      out.push(['p', `image ${i+1} of ${count}, page ${pageNum+1} of ${pages}`])
+    }
     return out
   }
 
-  function move (n) {
-    if (vs.offset >= 0) {
-      vs.offset += n
-      if (vs.offset < 0) vs.offset = 0
-      const itemsPerPage = vs.rows * vs.cols
-      if (vs.offset > (gm.slides.length - itemsPerPage)) {
-        vs.offset = gm.slides.length - itemsPerPage
-      }
-    } else {
-      vs.offset += n
-      // ...
-    }
-  }
 
 
   // http://stackoverflow.com/questions/6226859/how-can-i-track-arrow-keys-in-chrome-and-ie
@@ -199,68 +194,31 @@ function create () {
 
   document.onkeydown = (ev) => {
     const k = ev.key
-    if (k === 'ArrowLeft') {
-      if (vs.col === 0) {
-        if (vs.row === 0) {
-          vs.pageOffset--
-        } else {
-          vs.row--
-          vs.col = vs.cols - 1
-        }
-      } else {
-        vs.col--
+    const old = indexOfSelected()
+    let i = old
+
+    // perhaps these should also move the cursor, in an exactly predictable
+    // way, in case the data is changing under us...?
+    
+    if (k === 'ArrowLeft') i--
+    if (k === 'ArrowRight') i++
+    if (k === 'ArrowUp') i -= vs.cols
+    if (k === 'ArrowDown') i += vs.cols
+    if (k === 'PageUp') i -= (vs.rows * vs.cols)
+    if (k === 'PageDown') i  += (vs.rows * vs.cols)
+    if (k === 'Home') i = 0
+    if (k === 'End') i = slides.length - 1
+
+    if (i !== old) {
+      const item = slides[i]
+      if (item) {
+        config.selected = item
+        //vs.col = i % vs.cols
+        //vs.row = (i - vs.col) % vs.rows
       }
     }
 
-    if (k === 'ArrowRight') {
-      if (vs.col < vs.cols - 1) {
-        vs.col++
-      } else {
-        if (vs.row < vs.rows - 1) {
-          vs.row++
-          vs.col = 0
-        } else {
-          vs.pageOffset++
-        }
-      }
-    }
-
-    if (k === 'ArrowUp') {
-      if (vs.row === 0) {
-        vs.pageOffset -= vs.cols
-      } else {
-        vs.row--
-      }
-    }
-
-    if (k === 'ArrowDown') {
-      if (vs.row < vs.rows - 1) {
-        vs.row++
-      } else {
-        vs.pageOffset += vs.cols
-      }
-    }
-
-    if (k === 'PageUp') {
-      vs.pageOffset -= (vs.rows * vs.cols)
-    }
-
-    if (k === 'PageDown') {
-      vs.pageOffset += (vs.rows * vs.cols)
-    }
-
-    if (k === 'Home') {
-      vs.pageOffset = 0
-      vs.col = 0
-      vs.row = 0
-    }
-    if (k === 'End') {
-      vs.pageOffset = 0 - (vs.rows * vs.cols)
-      vs.col = vs.cols - 1
-      vs.row = vs.rows - 1
-      // which might be past end, if there aren't enough items
-    }
-
+    /* 
     if (k === '1') { vs.thumbsize = 12; setSize() }
     if (k === '2') { vs.thumbsize = 25; setSize() }
     if (k === '3') { vs.thumbsize = 50; setSize() }
@@ -270,12 +228,50 @@ function create () {
     if (k === '7') { vs.thumbsize = 150; setSize() }
     if (k === '8') { vs.thumbsize = 200; setSize() }
     // if (k === '9') { vs.thumbsize = 400; setSize() }
+    */
+    const kn = parseInt(k)
+    if ('' + kn === k && kn > 0) {
+      vs.rows = kn
+      setSize()
+    }
     
-    console.log('keydown', k)
-
-    updateSelection()
+    // console.log('keydown', k)
   }
 
+  let selectedAt = 0
+  function indexOfSelected () {
+    if (slides.length === 0) {
+      console.log('ERROR, slides array is empty')
+    }
+    if (slides[selectedAt] !== config.selected) {
+      selectedAt = slides.indexOf(config.selected)
+      if (selectedAt === -1) {
+        config.selected = slides[0]
+      }
+    }
+    return selectedAt
+  }
+  
+  // Where should the page start, for the selected item to be under the cursor?
+  function pageOffset () {
+    // const offset = indexOfSelected() - (vs.row * vs.cols + vs.col)
+    let i = indexOfSelected()
+    const pageSize = vs.rows * vs.cols
+    const pageNum = Math.floor(indexOfSelected() / pageSize)
+    const offset = pageNum * pageSize
+    return offset
+  }
+
+  /* For testing that UI is stable even under changing slides.
+
+     That is, the keyboard controls still move the current image around as expected, regardless of things being deleted....
+
+  setInterval( () => {
+    slides.splice(5, 1)
+    config.touch = slides.length
+  }, 1)
+  */
+  
   return thumbview
 }
 
